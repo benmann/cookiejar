ba50aa697585965fed546e58ca20860d7f865977
Ben Mann <mann@blanko.de>
2016-06-28 19:14:45 +0200

    Move to microservices
    This commit introduces microservices, separating the app into small
    services encapsulated with docker containers.
    The App is now split into:
    - collectors
    - queue
    - analyzers
    - api

--- config/elasticClient.js
+++
@@ -1,7 +0,0 @@
-var config = require('./config'),
-    elasticsearch = require('elasticsearch'),
-    elastic = elasticsearch.Client({
-      host: 'localhost:9200'
-    });
-
-module.exports = elastic;
--- config/thinky.js
+++
@@ -1,9 +0,0 @@
-var config = require('./config');
-var thinky = require('thinky')({
-  host: config.thinkHost,
-  port: config.thinkPort,
-  authKey: config.thinkAuthKey,
-  db: config.thinkDB
-});
-
-module.exports = thinky;
